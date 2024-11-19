d584f984452c0fdec59557e4ae21a8b796a23c8a
Helium_Studio <67852324+Helium-Studio@users.noreply.github.com>
2024-11-20 01:00:44 +0800

    rules: download rules into tmp dir

--- rules.sh
+++ rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /root
+cd /tmp
 curl -LO https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 curl -LO https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 echo "Latest rules downloaded!"
